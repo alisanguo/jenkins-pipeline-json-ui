7d8287333dee7e28bd5724f804ced91f2812837d
ali <ali@alideMacBook-Pro.local>
2020-03-02 14:10:34 +0800

    update demo

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import aceEditorTest from '@/components/aceEditorTest'
+import Index from '@/page/index'
+import Pipeline from '@/page/pipeline'
+
 
 Vue.use(Router)
 
 export default new Router({
+  base: '/cip/',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/ace-editor-test',
-      name: 'ace-editor-test',
-      component: aceEditorTest
+      name: 'index',
+      component: Index,
+      children: [
+        {
+          path: '/pipeline',
+          component: Pipeline
+        }
+      ]
     }
   ]
 })
